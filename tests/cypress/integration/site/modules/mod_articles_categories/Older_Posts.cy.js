e0f4e1beb6eaec38251fc9fdebe854d174d91c44
Charvi Mehra <121369234+charvimehradu@users.noreply.github.com>
2023-10-20 00:15:45 +0530

    Revert "Set up cypress"

--- tests/cypress/integration/site/modules/mod_articles_categories/Older_Posts.cy.js
+++
@@ -1,7 +0,0 @@
-describe('Test in frontend that the articles categories module', () => {
-  it('can display the "Older Posts" menu item', () => {
-    cy.visit('/');
-    cy.contains('Older Posts');
-    cy.get('.sidebar-left > .card-body');
-  });
-});
