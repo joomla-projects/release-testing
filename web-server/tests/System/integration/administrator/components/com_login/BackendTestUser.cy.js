1bb21414de9ce3bfdb53b0a578081830e007556b
Charvi Mehra <121369234+charvimehradu@users.noreply.github.com>
2024-01-26 23:04:30 +0530

    improve passwords

--- web-server/tests/System/integration/administrator/components/com_login/BackendTestUser.cy.js
+++ web-server/tests/System/integration/administrator/components/com_login/BackendTestUser.cy.js
@@ -2,11 +2,11 @@ describe('Test in backend that', () => {
   beforeEach(() => cy.doAdministratorLogin());
     
   it('can log in with a test user', () => {
-    cy.createSuperUser('test 010', 'test#123', 'testtesttest#123').then(() => {
+    cy.createSuperUser('test 010', 'test#123', 'TestReleaseTesting123!!').then(() => {
       cy.doAdministratorLogout();
       cy.visit('/administrator');
       cy.get('#mod-login-username').type('test#123');
-      cy.get('#mod-login-password').type('testtesttest#123');
+      cy.get('#mod-login-password').type('TestReleaseTesting123!!');
       cy.get('#btn-login-submit').click();    
     });
 
